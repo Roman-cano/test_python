657a6952a8606c5033dff02c079a03327e833923
roman <roman.cano04@gmail.com>
2025-10-07 15:07:58 +0200

    feat2

--- src/mon_premier_projet/utilitaire/math.py
+++ src/mon_premier_projet/utilitaire/math.py
@@ -1,14 +1,15 @@
 def multiply(a,b):
-	return a*b
+    return a*b
 
 
 def add(a,b):
-	return a+b
+    return a+b
 
 
 def sub(a,b):
-	return a-b
+    return a-b
 
 def divide(a,b):
-	return a/b
+    divide = a/b
+    return divide
 
