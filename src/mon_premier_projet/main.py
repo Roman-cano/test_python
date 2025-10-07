38b3814d34f4d741843c819c5085b5d0aea869d6
mrsxnshxne <jean.gabriel.trousseau12@gmail.com>
2025-10-07 16:31:33 +0200

    feat: add logic to main.py

--- src/mon_premier_projet/main.py
+++ src/mon_premier_projet/main.py
@@ -1,26 +1,19 @@
-from mon_premier_projet.utilitaire import math
-from mon_premier_projet.utilitaire import texte
-from mon_premier_projet.utilitaire import convert
 from mon_premier_projet.movies import movie
-
-
-def dire_coucou():
-    print("coucou !")
+from mon_premier_projet.movies.movie import get_movies, add_movie, print_movie
+from mon_premier_projet.notes import notes
+from mon_premier_projet.notes.notes import add_note, get_best_movie
 
 if __name__ == "__main__":
-    dire_coucou()
-    print(math.add(1 , 2))
-    print(texte.upper("roman"))
-    print(texte.lower("ROMAN"))
-    print(convert.yen_euro(5))
-    print(convert.euro_dollar(5))
-    print("hello")
     quit = 1
+
     while(quit != 0):
 
         print("1 ajouter un film")
         print("2 supprimer un film")
         print("3 afficher les  film")
+        print("4 ajouter une note à un film")
+        print("5 récupérer le nom du meilleur film")
+
         quit = int(input("quit?"))
 
         if(quit == 1):
@@ -35,8 +28,20 @@ if __name__ == "__main__":
             print("il n'y a aucun film a supprimer")
 
         if (quit == 3):
-            movie.get_movie()
+            print_movie()
 
+        if quit == 4:
+            movie_name = str(input("Indiquez le film à noter :"))
+            movie_note = float(input("Indiquez la note :"))
 
+            movies = get_movies()
+            if movie_name not in movies:
+                print("il n'y a aucun film a supprimer")
+            else:
+                add_note(movie_name, movie_note)
+                print("La note a bien été ajouté")
+
+        if quit == 5:
+            print(get_best_movie())
 
 
--- src/mon_premier_projet/movies/movie.py
+++ src/mon_premier_projet/movies/movie.py
@@ -5,10 +5,12 @@ def add_movie(film):
     movie.append(film)
 
 
-def get_movie():
+def print_movie():
     for i in range(len(movie)):
         print(i , movie[i])
 
+def get_movies():
+    return movie
 
 def delete_movie(index):
      movie.pop(index-1)
